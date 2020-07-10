a605d5b421d9e74b8e997ed13cb767599abca370
Larry Barrios Jr <gitlab@larrybarriosjr.com>
2020-07-10 12:34:40 +0800

    Add My Day page layout

--- src/my-day/MyDayPage.js
+++ src/my-day/MyDayPage.js
@@ -1,7 +1,25 @@
 import React from "react"
+import scss from "my-day/MyDayPage.module.scss"
 
 const MyDayPage = () => {
-  return <div>My Day</div>
+  return (
+    <div className={scss.background}>
+      <section className={scss.page}>
+        <h2 className={scss.title}>My Day</h2>
+        <p className={scss.date}>Friday, July 10</p>
+        <button className={scss.bulb}>
+          <i className="icon-lightbulb" />
+        </button>
+        <ul className={scss.list}>list</ul>
+        <form className={scss.form}>
+          <input placeholder="Add a task" />
+          <button type="button">
+            <i className="icon-plus" />
+          </button>
+        </form>
+      </section>
+    </div>
+  )
 }
 
 export default MyDayPage
