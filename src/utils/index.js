69d99608539c875602590b9e62464697480992a8
Larry Barrios Jr <gitlab@larrybarriosjr.com>
2020-08-11 15:23:12 +0800

    Add JSDoc comments on utils functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,30 @@
 import dayjs from "dayjs"
 import { Task } from "service/lovefield"
 
-// Day JS supplemental variables
+// Day JS supplemental variables (not formatted)
 export const currentHour = dayjs().startOf("hour")
 export const currentDay = dayjs().startOf("day")
 
-// Fetch Task API
-export const fetchTask = (id, setter) =>
+/**
+ * Fetch Task API accepting the task ID and the setter function, usually setTask
+ *
+ * @param {number} id
+ * @param {function} setterFunction setState
+ * @return {object} Updated task item data
+ *
+ */
+export const fetchTask = (id, setterFunction) =>
   Task.get(id)
-    .then((res) => setter(res))
+    .then((res) => setterFunction(res))
     .catch((err) => console.log(err))
 
+/**
+ * Regular debounce function with 500ms default delay
+ *
+ * @param {function} func
+ * @param {number} delay millisecond
+ * @return {function} Debounced function
+ */
 export const debounce = (func, delay = 500) => {
   let timeout
   return function () {
