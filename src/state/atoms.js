2543da53b19ba676f67334a4fc0c3f095308c256
Larry Barrios Jr <gitlab@larrybarriosjr.com>
2020-08-11 15:23:42 +0800

    Add comments on recoils atoms and selector

--- src/state/atoms.js
+++ src/state/atoms.js
@@ -1,51 +1,61 @@
 import { atom } from "recoil"
 import dayjs from "dayjs"
 
+/** Current page name. */
 export const pageState = atom({
   key: "pageState",
   default: "myDay"
 })
 
+/** Reminder modal with preset actions. */
 export const reminderModalState = atom({
   key: "reminderModalState",
   default: false
 })
 
+/** Calendar modal for the reminder. */
 export const reminderCalendarModalState = atom({
   key: "reminderCalendarModalState",
   default: false
 })
 
+/** Current date chosen in calendar modals. */
 export const dateState = atom({
   key: "dateState",
   default: dayjs().startOf("day")
 })
 
+/** Current hour chosen in time picker. */
 export const hourState = atom({
   key: "hourState",
   default: dayjs().format("hh")
 })
 
+/** Current minute chosen in time picker. */
 export const minuteState = atom({
   key: "minuteState",
   default: dayjs().format("mm")
 })
 
+/** Current period chosen in time picker. */
 export const periodState = atom({
   key: "periodState",
   default: dayjs().format("A")
 })
 
+/** Task drawer display. */
 export const taskHiddenState = atom({
   key: "taskHiddenState",
   default: true
 })
 
+/** List of all task items (unfiltered). */
 export const taskListState = atom({
   key: "taskList",
   default: []
 })
 
+/** Data of the current task item chosen. */
 export const taskState = atom({
   key: "taskState",
   default: {}
--- src/state/selectors.js
+++ src/state/selectors.js
@@ -1,20 +1,24 @@
 import { selector } from "recoil"
 import { pageState, taskListState } from "state/atoms"
 
+/** List of task items depending on current page. */
 export const pageListState = selector({
   key: "pageListState",
   get: ({ get }) => {
-    const page = get(pageState)
-    const taskList = get(taskListState)
+    const page = get(pageState) // get page name
+    const taskList = get(taskListState) // get all task items (unfiltered)
     if (taskList.length) {
       switch (page) {
+        // return all task items with My Day only
         case "myDay":
           return taskList.filter((task) => task.myDay)
+
+        // return all task items not belonging to a list
         default:
           return taskList.filter((task) => task.listId === null)
       }
     } else {
-      return []
+      return [] // empty task list
     }
   }
 })
