0be88da0dbff952f848275834e9f3ca1e0c68acb
Larry Barrios Jr <gitlab@larrybarriosjr.com>
2020-08-16 20:16:14 +0800

    Add date indicators in Task Item

--- src/common/TaskItem.js
+++ src/common/TaskItem.js
@@ -5,10 +5,14 @@ import scss from "common/TaskItem.module.scss"
 import CheckButton from "./CheckButton"
 import StarButton from "./StarButton"
 import { fetchTask } from "utils"
+import dayjs from "dayjs"
 
 const TaskItem = ({ item }) => {
   const [task, setTask] = useRecoilState(taskState) // Task item
   const [taskHidden, setTaskHidden] = useRecoilState(taskHiddenState) // Drawer display status
+
+  const bullet = "\u2022"
+
   const setStepList = useSetRecoilState(stepListState)
 
   const toggleTaskDrawer = (id) => () => {
@@ -24,6 +28,35 @@ const TaskItem = ({ item }) => {
     })
   }
 
+  const StepsIndicator = () => {
+    let icon = ""
+    if (item.stepsCompleted === item.stepsTotal) {
+      icon = <i className="icon-ok" />
+    }
+    return (
+      <>
+        {" "}
+        {bullet} {icon} {item.stepsCompleted} of {item.stepsTotal}
+      </>
+    )
+  }
+  const DateIndicator = ({ date, dueDate, reminder }) => {
+    let icon = ""
+    if (dueDate) icon = <i className="icon-calendar-check-o" />
+    if (reminder) icon = <i className="icon-bell" />
+    const timeDiff = (dt) => {
+      if (dayjs().isSame(dt, "day")) return "Today"
+      if (dayjs().add(1, "day").isSame(dt, "day")) return "Tomorrow"
+      return dayjs(dt).format("ddd, MMM D")
+    }
+    return (
+      <>
+        {" "}
+        {bullet} {icon} {timeDiff(date)}
+      </>
+    )
+  }
+
   // Style: Strikethrough task name when completed
   const itemNameClass = `${scss["item-name"]} ${item.completed && scss.deleted}`
 
@@ -36,8 +69,10 @@ const TaskItem = ({ item }) => {
       />
       <p className={itemNameClass}>{item.name}</p>
       <p className={scss["item-category"]}>
-        {item.listId || "Tasks"}{" "}
-        {item.stepsTotal ? `${item.stepsCompleted} of ${item.stepsTotal}` : ""}
+        {item.listId || "Tasks"}
+        {item.stepsTotal > 0 && <StepsIndicator />}
+        {item.dueDate && <DateIndicator date={item.dueDate} dueDate />}
+        {item.reminder && <DateIndicator date={item.reminder} reminder />}
       </p>
       <StarButton
         id={item.id}
