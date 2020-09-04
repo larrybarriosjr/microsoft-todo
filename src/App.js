a881a7d6afe532f7634225f4beecf66fff1863dd
Larry Barrios Jr <github@larrybarriosjr.com>
2020-09-04 18:27:52 +0800

    Testing PWA actions

--- src/App.js
+++ src/App.js
@@ -16,7 +16,25 @@ import { Task } from "service/lovefield"
 import dayjs from "dayjs"
 
 function App() {
-  window.self.addEventListener("notificationclick", (e) => {
+  window.self.addEventListener("notificationclick", async (e) => {
+    const registration = await window.navigator.serviceWorker.getRegistration()
+    const options = {
+      tag: e.tag,
+      icon: "./favicon.ico",
+      badge: "./favicon.ico",
+      body: e.name,
+      showTrigger: new window.TimestampTrigger(dayjs(e.tag).valueOf()),
+      actions: [
+        { action: "snooze-action", title: "Snooze 30 mins." },
+        { action: "dismiss-action", title: "Dismiss" }
+      ],
+      data: {
+        id: e.id,
+        url: window.location.href,
+        name: e.name
+      },
+      requireInteraction: true
+    }
     switch (e.action) {
       case "snooze-action":
         console.log("ID: " + e.id)
@@ -24,7 +42,10 @@ function App() {
         console.log("URL: " + e.url)
         console.log("Snooze 30 minutes")
         window.location.href = e.url
-        Task.patch({ id: e.id, reminder: new Date(dayjs().add(10, "second")) })
+        Task.patch({
+          id: e.id,
+          reminder: new Date(dayjs().add(10, "second"))
+        }).then(() => registration.showNotification("Task Reminder", options)) // Set PWA Notification
         break
       case "dismiss-action":
         console.log("ID: " + e.id)
